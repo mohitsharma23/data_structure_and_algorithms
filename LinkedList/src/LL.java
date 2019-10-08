2b2118c3fbaa9f49b5f8fb12e68e7f89e692a031
mohitsharma23 <mohitsharma23j97@gmail.com>
2019-10-08 23:56:38 +0530

    Delete and reverse

--- LinkedList/src/LL.java
+++ LinkedList/src/LL.java
@@ -14,6 +14,18 @@ public class LL {
 	
 	public static class LLDriver{
 		Node head;
+		
+		int getLength() {
+			int c = 0;
+			Node curr = head;
+			while(curr != null) {
+				c++;
+				curr = curr.next; 
+			}
+			
+			return c;
+		}
+		
 		void InsertFront(int d) {
 			Node node = new Node(d);
 			if(head == null) {
@@ -49,6 +61,49 @@ public class LL {
 			curr.next = node;
 		}
 		
+		void DeleteFront() {
+			Node curr = head;
+			if(curr != null) {
+				head = curr.next;
+			}
+		}
+		
+		void DeleteEnd() {
+			Node curr = head;
+			while(curr.next.next != null) {
+				curr = curr.next;
+			}
+			
+			curr.next = null;
+		}
+		
+		void DeletePosition(int p) {
+			Node curr = head, prev = null;
+			if(p == 1) {
+				head = curr.next;
+			}else {
+				while(p-1 != 0) {
+					prev = curr;
+					curr = curr.next;
+					p--;
+				}
+				prev.next = curr.next;
+				curr.next = null;
+			}
+			
+		}
+		
+		void Reverse() {
+			Node curr = head, prev = null, next = null;
+			while(curr != null) {
+				next = curr.next;
+				curr.next = prev;
+				prev = curr;
+				curr = next;
+			}
+			head = prev;
+		}
+		
 		void display() {
 			Node curr = head;
 			while(curr != null) {
@@ -72,7 +127,8 @@ public class LL {
 			System.out.println("6. Delete at position");
 			System.out.println("7. Reverse");
 			System.out.println("8. Display");
-			System.out.println("9. Exit");
+			System.out.println("9. Get Length");
+			System.out.println("10. Exit");
 			System.out.println("Enter choice:");
 			choice = sc.nextInt();
 			switch(choice) {
@@ -92,12 +148,30 @@ public class LL {
 				pos = sc.nextInt();
 				ll.InsertPosition(data, pos);
 				break;
+			case 4:
+				ll.DeleteFront();
+				break;
+			case 5:
+				ll.DeleteEnd();
+				break;
+			case 6:
+				System.out.print("Enter the position to delete: ");
+				pos = sc.nextInt();
+				ll.DeletePosition(pos);
+				break;
+			case 7:
+				ll.Reverse();
+				break;
 			case 8:
 				ll.display();
 				break;
+			case 9:
+				int len = ll.getLength();
+				System.out.println("Length of Linked List is: "+len);
+				break;
 			}
 			
-		}while(choice != 9);
+		}while(choice != 10);
 
 	}
 }
